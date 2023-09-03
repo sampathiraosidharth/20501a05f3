340516a6cd53eb9e50f7586904348054f2342058
SAMPATHI RAO SIDHARTH <sampathisidharth1603@gmail.com>
2023-09-03 12:19:01 +0530

    updated the first question done , task

--- number_management_app/my-express-server/server.js
+++ number_management_app/my-express-server/server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const app = express();
 
 // Allow requests from your React app's domain (replace with your actual domain)
-const allowedOrigins = ['http://localhost:3000','http://localhost:3001','http://localhost:3002'];
+const allowedOrigins = ['http://localhost:3000','http://localhost:3001','http://localhost:3002']; // Update this with your React app's domain
 
 app.use(
   cors({
@@ -13,44 +13,59 @@ app.use(
   })
 );
 
-
-app.get('/merged-numbers', async (req, res) => {
+// Define routes for the provided URLs
+app.get('/numbers/primes', async (req, res) => {
   try {
-    
-    const urls = [
-      'http://20.244.56.144/numbers/primes',
-      'http://20.244.56.144/numbers/fibo',
-      'http://20.244.56.144/numbers/odd',
-      'http://20.244.56.144/numbers/rand',
-    ];
-
-    
-    const responses = await Promise.all(urls.map((url) => axios.get(url)));
-
-    
-    const allNumbers = responses
-      .map((response) => response.data.numbers)
-      .flat();
-
-   
-    const uniqueNumbers = [...new Set(allNumbers)];
-
-    // Sort the unique integers in ascending order
-    const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
-
-    // Set the Access-Control-Allow-Origin header to allow requests from your React app's domain
-    res.header('Access-Control-Allow-Origin', allowedOrigins[0]); // Use the first allowed origin
-
-    // Send the merged numbers as JSON response
-    res.json({ numbers: sortedNumbers });
+    // Fetch data from the external API (http://20.244.56.144/numbers/primes)
+    const response = await axios.get('http://20.244.56.144/numbers/primes');
+    const numbers = response.data.numbers;
+    res.json({ numbers });
   } catch (error) {
-    console.error('Error fetching and merging data:', error);
+    console.error('Error fetching data from /numbers/primes:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/numbers/fibo', async (req, res) => {
+  try {
+    // Fetch data from the external API (http://20.244.56.144/numbers/fibo)
+    const response = await axios.get('http://20.244.56.144/numbers/fibo');
+    const numbers = response.data.numbers;
+    res.json({ numbers });
+  } catch (error) {
+    console.error('Error fetching data from /numbers/fibo:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/numbers/odd', async (req, res) => {
+  try {
+    // Fetch data from the external API (http://20.244.56.144/numbers/odd)
+    const response = await axios.get('http://20.244.56.144/numbers/odd');
+    const numbers = response.data.numbers;
+    res.json({ numbers });
+  } catch (error) {
+    console.error('Error fetching data from /numbers/odd:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/numbers/rand', async (req, res) => {
+  try {
+    // Fetch data from the external API (http://20.244.56.144/numbers/rand)
+    const response = await axios.get('http://20.244.56.144/numbers/rand');
+    const numbers = response.data.numbers;
+    res.json({ numbers });
+  } catch (error) {
+    console.error('Error fetching data from /numbers/rand:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 // Your server setup here...
 
-app.listen(8008, () => {
-  console.log('Server is running on port 8008');
+const PORT = process.env.PORT || 8008; // Use the specified PORT or default to 8008
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- number_management_app/src/Numbers.js
+++ number_management_app/src/Numbers.js
@@ -1,29 +1,87 @@
 import React, { useEffect, useState } from 'react';
+import './index';
 
 const App = () => {
   const [mergedNumbers, setMergedNumbers] = useState([]);
 
   useEffect(() => {
-    // Make a GET request to your Express.js server (running on port 8008)
-    fetch('http://localhost:8008/merged-numbers')
-      .then((response) => response.json())
-      .then((data) => {
-        setMergedNumbers(data.numbers);
+    // Define the URLs to fetch data from
+    const urls = [
+      'http://localhost:8008/numbers/primes',
+      'http://localhost:8008/numbers/fibo',
+      'http://localhost:8008/numbers/odd',
+      'http://localhost:8008/numbers/rand',
+    ];
+
+    // Make GET requests to all the URLs in parallel
+    Promise.all(
+      urls.map((url) =>
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => data.numbers)
+          .catch((error) => {
+            console.error(`Error fetching data from ${url}:`, error);
+            return [];
+          })
+      )
+    )
+      .then((responses) => {
+        // Merge and sort the data
+        const mergedData = responses.flat().filter((number) => typeof number === 'number');
+        const uniqueNumbers = [...new Set(mergedData)];
+        const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+        setMergedNumbers(sortedNumbers);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error merging data:', error);
       });
   }, []);
 
   return (
-    <div>
-      <h1>Merged Unique Integers</h1>
-      <ul>
-        {mergedNumbers.map((number) => (
-          <li key={number}>{number}</li>
-        ))}
-      </ul>
-    </div>
+    // <div className="min-h-screen bg-gray-100 p-4">
+    //   <h1 className="text-2xl font-semibold mb-4">Merged Unique Integers</h1>
+    //   <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    //     {mergedNumbers.map((number) => (
+    //       <li
+    //         key={number}
+    //         className="bg-white rounded-lg p-4 shadow-md transition-transform transform hover:scale-105"
+    //       >
+    //         {number}
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+    <div style={{ minHeight: '100vh', backgroundColor: '#f0f0f0', padding: '1rem' }}>
+  <h1 style={{ fontSize: '1.25rem', fontWeight: '600', marginBottom: '1rem' }}>Merged Unique Integers</h1>
+  <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
+    {mergedNumbers.map((number) => (
+      <div
+        key={number}
+        style={{
+          backgroundColor: '#ffffff',
+          borderRadius: '0.25rem',
+          padding: '1rem',
+          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+          transition: 'transform 0.2s',
+          transformOrigin: 'center',
+          cursor: 'pointer',
+          display: 'inline-block',
+          margin: '0.25rem',
+        }}
+        onMouseOver={(e) => {
+          e.target.style.transform = 'scale(1.05)';
+        }}
+        onMouseOut={(e) => {
+          e.target.style.transform = 'scale(1)';
+        }}
+      >
+        {number}
+      </div>
+    ))}
+  </div>
+</div>
+
+
   );
 };
 
