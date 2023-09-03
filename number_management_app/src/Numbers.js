6211409d3f892c89040c8cfdb7df6e2cf30185e8
SAMPATHI RAO SIDHARTH <sampathisidharth1603@gmail.com>
2023-09-03 11:49:00 +0530

    added styling

--- number_management_app/src/Numbers.js
+++ number_management_app/src/Numbers.js
@@ -15,39 +15,44 @@ const NumberList = () => {
 
     // get the data
     const fetchData = async () => {
-      try {
-        const responses = await Promise.all(
-          urls.map((url) => axios.get(`http://localhost:8008/numbers?url=${url}`))
-        );
+      const promises = urls.map(async (url) => {
+        try {
+          const response = await axios.get(url, { timeout: 500 });
+          return response.data.numbers;
+        } catch (error) {
+          console.error(`Error fetching data from ${url}:`, error);
+          return [];
+        }
+      });
+
+      const responses = await Promise.all(promises);
 
         // main function , where it extracts and merges the numbers 
         const allNumbers = responses
-          .map((response) => response.data.numbers)
-          .flat();
+        .flatMap((response) => response)
+        .filter((number) => typeof number === 'number');
 
-        // am using set as the data structure because , only one entry is possible 
-        const uniqueNumbers = [...new Set(allNumbers)];
-        const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+      const uniqueNumbers = [...new Set(allNumbers)];
+      const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
 
-        setMergedNumbers(sortedNumbers);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      setMergedNumbers(sortedNumbers);
     };
+
 
     fetchData();
   }, []);
 
   return (
-    <div>
-      <h1>Merged Unique Integers</h1>
-      <ul>
+    <div className="App">
+      <h1 className="text-xl font-bold mb-4 ">Merged Unique Integers</h1>
+      <ul className="list-disc pl-4">
         {mergedNumbers.map((number) => (
-          <li key={number}>{number}</li>
+          <li key={number} className="text-lg mb-2">
+            {number}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
-
 export default NumberList;
